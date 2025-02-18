c56e1e0359887b2869d1a8abe1ed6837c2c780e6
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-02-18 18:43:48 -0400

    feat: adicionar exemplos de variáveis, constante e tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double minimumWage = 2500;
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+
+
+        int number = 10;
+
+        number = 20;
+
+        System.out.println(number);
+
+        final double PI_VALUE = 3.14;
     }
 }
